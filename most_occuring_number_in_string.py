f917660aa23c1e19e764cdeb7f7267b44807ed1e
Abhishek2018DS <inquisitivereckoner@gmail.com>
2020-12-23 10:10:04 +0530

    most occuring number in the string

---
+++ most_occuring_number_in_string.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 22 21:34:45 2020
+
+@author: abhishek
+"""
+"""
+Input :geek55of55geeks4abc3dr2 
+Output :55
+
+Input :abcd1def22high22bnasvd23vjhd44
+Output :22
+"""
+import re
+from collections import Counter
+
+def most_occur_num(string):
+    max_m= 0
+    max_element= 0
+    arr= re.findall(r"[0-9]+", string)
+    c= Counter(arr)
+    
+    for i in c.keys():
+        if c[i]>= max_m:
+            max_m= c[i]
+            max_element= int(i)
+            
+    return max_element
+
+#driver code
+if __name__=="__main__":
+    string= "abcd1def22high22bnasvd23vjhd44"
+    print(most_occur_num(string))
+    
+    
+    
